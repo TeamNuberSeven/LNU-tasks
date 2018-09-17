ef76db03e962b219b72509521ce92c58bee3d4c6
RudenkoOleksii <oleksii98@gmail.com>
2018-09-17 17:11:01 +0300

    edit classes according to comments

---
+++ First_task/First_task/source/Student.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace First_task.source
+{
+	class Student : Person
+	{
+		private string _facility;
+		private string _teacherId;
+
+		public string Facility
+		{
+			get => _facility;
+			set => _facility = value;
+		}
+
+		internal string TeacherId
+		{
+			get => _teacherId;
+			set => _teacherId = value;
+		}
+
+		public Student()
+		{
+			Facility = string.Empty;
+			TeacherId = string.Empty;
+		}
+
+		public Student(string name, int age, string id, string facility, string teacherId) : base(name, age,id)
+		{
+			Facility = facility;
+			TeacherId = teacherId;
+		}
+
+		public override string ToString()
+		{
+			return $"\nName: {Name}, Age: {Age}, Facility {Facility}";
+		}
+
+		public override void Print()
+		{
+			base.Print();
+			Console.WriteLine($"\nFacility: {0}", Facility);
+		}
+
+		public override void Input(string data)
+		{
+			base.Input(data);
+			var words = data.Split(' ');
+			Facility = words[2];
+			TeacherId = words[3];
+		}
+
+		public override bool Equals(object obj)
+		{
+			var student = obj as Student;
+			return student != null &&
+			       base.Equals(obj) &&
+			       Facility == student.Facility &&
+			       EqualityComparer<string>.Default.Equals(TeacherId, student.TeacherId);
+		}
+
+		public override int GetHashCode()
+		{
+			var hashCode = 1568470154;
+			hashCode = hashCode * -1521134295 + base.GetHashCode();
+			hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(Facility);
+			hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(TeacherId);
+			return hashCode;
+		}
+
+		public static bool operator ==(Student student1, Student student2)
+		{
+			return EqualityComparer<Student>.Default.Equals(student1, student2);
+		}
+
+		public static bool operator !=(Student student1, Student student2)
+		{
+			return !(student1 == student2);
+		}
+	}
+}
---
+++ First_task/First_task/source/Teacher.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace First_task.source
+{
+	class Teacher : Person
+	{
+		private string _degree;
+		private string _subject;
+		private List<string> _studentsId;
+
+		public string Degree
+		{
+			get => _degree;
+			set => _degree = value;
+		}
+
+		public string Subject
+		{
+			get => _subject;
+			set => _subject = value;
+		}
+
+		internal List<string> StudentsId
+		{
+			get => _studentsId;
+			set => _studentsId = value;
+		}
+
+		public Teacher()
+		{
+			Degree = string.Empty;
+			Subject = string.Empty;
+			StudentsId = new List<string>();
+		}
+
+		public Teacher(string name, int age,string id, string degree, string subject) : base(name, age,id)
+		{
+			Degree = degree;
+			Subject = subject;			
+		}
+
+		public override string ToString()
+		{
+			return $"\nName: {Name}, Age: {Age}, Degree {Degree}, Subject: {Subject}" +
+			       StudentsId.Aggregate("", (current, student) => current + " " + student);
+		}
+
+		public void AssignStudents(List<Student> students)
+		{
+			foreach (Student stud in students)
+			{
+				StudentsId.Add(stud.Id);
+				stud.TeacherId = Id;
+			}
+		}
+		public override void Print()
+		{
+			base.Print();
+			Console.WriteLine($"\nDegree: {0} \nSubject: {1}", Degree, Subject);
+			foreach (var student in StudentsId)
+			{
+				Console.WriteLine(student);
+			}
+		}
+
+		public override void Input(string data)
+		{
+			base.Input(data);
+			var words = data.Split(' ');
+			Degree = words[2];
+			Subject = words[3];
+			for (int i = 4; i < words.Length; i++)
+			{
+				StudentsId.Add(words[i]);
+			}
+		}
+
+		public override bool Equals(object obj)
+		{
+			var teacher = obj as Teacher;
+			return teacher != null &&
+			       base.Equals(obj) &&
+			       Degree == teacher.Degree &&
+			       Subject == teacher.Subject &&
+			       EqualityComparer<List<string>>.Default.Equals(StudentsId, teacher.StudentsId);
+		}
+
+		public override int GetHashCode()
+		{
+			var hashCode = 1332502943;
+			hashCode = hashCode * -1521134295 + base.GetHashCode();
+			hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(Degree);
+			hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(Subject);
+			hashCode = hashCode * -1521134295 + EqualityComparer<List<string>>.Default.GetHashCode(StudentsId);
+			return hashCode;
+		}
+
+		public static bool operator ==(Teacher teacher1, Teacher teacher2)
+		{
+			return EqualityComparer<Teacher>.Default.Equals(teacher1, teacher2);
+		}
+
+		public static bool operator !=(Teacher teacher1, Teacher teacher2)
+		{
+			return !(teacher1 == teacher2);
+		}
+	}
+}
