1955c3ab800b260c5836619a8dd2df1054a2b34d
Oleksii <oleksii98@gmail.com>
2018-09-19 00:36:51 +0300

    Add a function that checks Equals and add function that counts Students-Teacher

--- First_task/First_task/source/domain/use_cases/PersonLogic.cs
+++ First_task/First_task/source/domain/use_cases/PersonLogic.cs
@@ -1,12 +1,41 @@
-﻿using System;
+﻿using First_task.source.data;
+using First_task.source.domain.entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace First_task.source.domain.use_cases
 {
-    class PersonLogic
+    public class PersonLogic
     {
+        private readonly IPersonRepository _repo;
+
+        public PersonLogic(IPersonRepository repo)
+        {
+            _repo = repo;
+        }
+
+        public void DemonstrateEquals()
+        {
+            IEnumerable<Person> distinctPersons = _repo.Fetch().Distinct();
+            for(int i =0;i <distinctPersons.Count() - 1; i++)
+            {
+                if(distinctPersons.ElementAt(i).Equals(distinctPersons.ElementAt(i+1)))
+                {
+                    throw new Exception("Methods Equals does not work correctly");
+                }
+            }
+        }
+        public string CountPerson()
+        {
+            int countStudents;
+            int countTeachers;
+            countStudents = _repo.Fetch().Where(p => p is Student).Count();
+            countTeachers = _repo.Fetch().Where(p => p is Teacher).Count();
+            string result;
+            result = string.Format("Amount of student {0} and amount of teacher: {1}", countStudents, countTeachers);
+            return result;
+        }
     }
 }
