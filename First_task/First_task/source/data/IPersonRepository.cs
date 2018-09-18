efdf0b45e94f67cab6f3926f4c00d11054111346
Oleksii <oleksii98@gmail.com>
2018-09-18 23:59:24 +0300

    Create interface IPersonRepository

---
+++ First_task/First_task/source/data/IPersonRepository.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using First_task.source.domain.entities;
+
+namespace First_task.source.data
+{
+    public interface IPersonRepository
+    {
+        List<Person> Fetch();
+    }
+}
