e365d7eb6bbef136040217d6c31d460a4505adf3
maksgr <maksgribun@gmail.com>
2018-09-20 18:49:33 +0300

    add unit tests

---
+++ First_task/First_task_test/DomainTest.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections.Generic;
+using First_task.source.data;
+using First_task.source.domain.entities;
+using First_task.source.domain.use_cases;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NSubstitute;
+
+namespace First_task_test
+{
+    [TestClass]
+    public class DomainTest
+    {
+        private List<Person> _mockPersons;
+        private IPersonRepository repo;
+        private const int AMOUNT_OF_UNIQUE_ELEMENTS = 4;
+        private const int AMOUNT_OF_STUDENTS = 3;
+        private const int AMOUNT_OF_TEACHERS = 3;
+        private const string AMOUNT = "Amount of student ";
+        private const string TEACHER_AMOUNT = "and amount of teacher: ";
+
+        [TestInitialize]
+        public void Init()
+        {
+            _mockPersons = new List<Person>
+            {
+                new Teacher("name1", 1, "1", "1", "1"),
+                new Student("name3", 3, "3", "3", "3"),
+                new Teacher("name2", 2, "2", "2", "2"),
+                new Student("name4", 4, "4", "4", "4"),
+                new Student("name3", 3, "3", "3", "3"),
+                new Teacher("name2", 2, "2", "2", "2"),
+            };
+            repo = Substitute.For<IPersonRepository>();
+            repo.Fetch().Returns(_mockPersons);
+        }
+
+        [TestMethod]
+        public void FetchPersonTest()
+        {
+            var fetchPersons = new FetchPersons(repo).Fetch();
+            Assert.AreEqual(_mockPersons[0], fetchPersons[0]);
+            Assert.AreEqual(_mockPersons[3], fetchPersons[3]);
+        }
+
+        [TestMethod]
+        public void FetchUniqueListOfPersonsTest()
+        {
+            var uniquePersons = new FetchUnigueListOfPersons(repo).FetchUnique();
+            Assert.AreEqual(uniquePersons.Count, AMOUNT_OF_UNIQUE_ELEMENTS);
+        }
+
+        [TestMethod]
+        public void CountAmountOfPersons()
+        {
+            var personsAmount = new CountAmountOfPersons(repo).CountPerson();
+            var resultString = string.Format(AMOUNT + AMOUNT_OF_STUDENTS + TEACHER_AMOUNT + AMOUNT_OF_TEACHERS);
+            Assert.AreEqual(personsAmount, resultString);
+        }
+    }
+}
